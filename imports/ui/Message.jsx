2a5d08bd0df72d5fcfe0887742b8c6856dbe267d
dalthviz <d.althviz10@uniandes.edu.co>
2017-03-23 23:45:45 -0500

    Mejoras en la distribución de contenido mensaje finalización.

--- imports/ui/Message.jsx
+++ imports/ui/Message.jsx
@@ -26,17 +26,23 @@ export default class Message extends Component {
         </div>
         </div>
         <div className="row">
-          <div className="col-xs-8 col-sm-6 col-md-3-offset">
+          <div className="col-xs-6 col-sm-6 ">
             {this.props.game.winner != 'tie'?
-            <h1>Winner!!!<span className="red"></span>: {this.props.game.winner}</h1>:
-            <h1>It was a {this.props.game.winner}!!!<span className="red"></span></h1>
-            }
+            <h2>Winner!!!<span className="red"></span>: {this.props.game.winner}</h2>
+            :<h2>It was a {this.props.game.winner}!!!<span className="red"></span></h2>}
+
             {this.props.game.winner === Meteor.user().username
             ?<h3>You Won!</h3>: this.props.game.winner != 'tie'? <h3>Maybe the next time ;)</h3>:<h3>Good game!</h3>}
-            <h3>Score for {this.props.game.p1.username}: {this.props.game.p1.score}</h3>
-            <h3>Score for {this.props.game.p2.username}: {this.props.game.p2.score}</h3>
           </div>
-
+          <div className="col-xs-6 create-game">
+            <h4>Scores:</h4>
+            <div className="row">
+              <div className="col-xs-12">
+                <h4>Score for {this.props.game.p1.username}: {this.props.game.p1.score}</h4>
+                <h4>Score for {this.props.game.p2.username}: {this.props.game.p2.score}</h4>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div className="row">
